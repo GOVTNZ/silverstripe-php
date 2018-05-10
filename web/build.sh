0bee5152c12d988d4df6d2b3537d5ee0f7eb9e50
Juan van den Anker <juan@vdanker.net>
2018-05-10 20:21:30 +1200

    Renamed container

--- web/build.sh
+++ web/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker image rm govtnz/silverstripe-php:1.0
-docker build -t govtnz/silverstripe-php:1.0 .
+docker image rm govtnz/silverstripe-web-container:1.0
+docker build -t govtnz/silverstripe-web-container:1.0 .
