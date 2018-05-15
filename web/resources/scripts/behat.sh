3fcafbf0694d704ea6046d5629f52aac7bce4761
Juan van den Anker <juan@vdanker.net>
2018-05-15 21:45:50 +1200

    Set composer home

--- web/resources/scripts/behat.sh
+++ web/resources/scripts/behat.sh
@@ -2,6 +2,7 @@
 
 HOST_UID=$(stat -c %u /var/www/html)
 HOST_GID=$(stat -c %g /var/www/html)
+export COMPOSER_HOME=/cache/.composer
 
 cd /var/www/html
 
--- web/resources/scripts/shell.sh
+++ web/resources/scripts/shell.sh
@@ -2,5 +2,6 @@
 
 HOST_UID=$(stat -c %u /var/www/html)
 HOST_GID=$(stat -c %g /var/www/html)
+export COMPOSER_HOME=/cache/.composer
 
 exec gosu $HOST_UID:$HOST_GID /bin/bash
