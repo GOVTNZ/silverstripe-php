e7825c929d8117be06fce4429b7745d831dd4c7a
Juan van den Anker <juan@vdanker.net>
2018-05-15 16:38:31 +1200

    Added option for behat-docker.yml config file

--- web/resources/scripts/behat.sh
+++ web/resources/scripts/behat.sh
@@ -3,4 +3,11 @@
 HOST_UID=$(stat -c %u /var/www/html)
 HOST_GID=$(stat -c %g /var/www/html)
 
-exec gosu $HOST_UID:$HOST_GID php vendor/bin/behat $@
+cd /var/www/html
+
+if [ -f "behat-docker.yml" ]; then
+  exec gosu $HOST_UID:$HOST_GID php vendor/bin/behat -c behat-docker.yml $@
+else
+  exec gosu $HOST_UID:$HOST_GID php vendor/bin/behat $@
+fi
+
